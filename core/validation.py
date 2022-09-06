b04ec28bee5dc6a04d1a1477ad8c34f56c0a307c
minha <miraculum95@gmail.com>
2022-09-06 22:43:50 +0900

    Feat: validation 추가

---
+++ core/validation.py
@@ -0,0 +1,10 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+# 비밀번호는 6자 이상, 숫자 1개 이상 포함되어야 함
+PASSWORD_REGEX = '(?=.*\d)[A-Za-z\d$@^!%*#?&]{6,}$'
+
+def validate_password(value):
+    if not re.match(PASSWORD_REGEX,value):
+        raise ValidationError('Invalid Password')
