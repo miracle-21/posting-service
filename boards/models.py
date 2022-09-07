7ed8f883271eb270f1e0fd6d544137b2ce632ca8
minha <miraculum95@gmail.com>
2022-09-07 20:32:41 +0900

    Style: 주석 추가

--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# 게시판 모델링 
 class Board(models.Model):
     title       = models.CharField(max_length=20)
     context     = models.CharField(max_length=200)
--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,3 @@
-from contextvars import Context
 import json
 import bcrypt
 
@@ -10,6 +9,11 @@ from core.validation import validate_password
 from boards.models import Board
 
 class CreateView(View):
+    """
+    * 게시물 생성: 제목, 내용, 비밀번호 필요
+    * @POST ("/boards")
+    * @returns json
+    """
     def post(self, request):
         try:
             data    = json.loads(request.body)
@@ -18,8 +22,10 @@ class CreateView(View):
             context = data['context']
             passwd  = data['passwd']
 
+            # 비밀번호 유효성 검사
             validate_password(passwd)
 
+            # 비밀번호 암호화
             hashed_password  = bcrypt.hashpw(passwd.encode('utf-8'), bcrypt.gensalt())
             
             Board.objects.create(
@@ -33,11 +39,16 @@ class CreateView(View):
             return JsonResponse({'message' : error.message}, status = 400)
 
 class DeleteView(View):
+    """
+    * 게시물 삭제: 비밀번호 필요
+    * @DELETE ("/boards/delete/:id")
+    * @returns json
+    """
     def delete(self, request, id):
-        data = json.loads(request.body)
-
+        data  = json.loads(request.body)
         board = Board.objects.get(id=id)
 
+        # 비밀번호 비교, 일지하지 않으면 에러 메세지 반환.
         if not bcrypt.checkpw(data['passwd'].encode('utf-8'), board.passwd.encode('utf-8')):
             return JsonResponse({'message' : 'invalid password'}, status = 401)
         else:
@@ -46,19 +57,24 @@ class DeleteView(View):
 
 
 class UpdateView(View):
+    """
+    * 게시물 수정: 비밀번호 필요
+    * @PATCH ("/boards/update/:id")
+    * @returns json
+    """
     def patch(self, request, id):
-        data = json.loads(request.body)
+        data  = json.loads(request.body)
         board = Board.objects.get(id=id)
 
+        # 비밀번호 비교, 일지하지 않으면 에러 메세지 반환.
         if not bcrypt.checkpw(data['passwd'].encode('utf-8'), board.passwd.encode('utf-8')):
             return JsonResponse({'message' : 'invalid password'}, status = 401)
         else:
-            data = json.loads(request.body)
-            
+            data  = json.loads(request.body)
             board = Board.objects.get(id=id)
 
             if 'title' in data.keys():
-                board.title = data['title']
+                board.title   = data['title']
             if 'context' in data.keys():
                 board.context = data['context']
             
